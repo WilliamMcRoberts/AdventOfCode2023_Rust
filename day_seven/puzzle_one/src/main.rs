c16ac75e293a819f411ce6c148b10122237d32e4
WilliamMcRoberts <wamcroberts2@gmail.com>
2023-12-09 00:14:55 -0500

    day 7  puzzle one fix

--- day_seven/puzzle_one/src/main.rs
+++ day_seven/puzzle_one/src/main.rs
@@ -1,4 +1,3 @@
-#![feature(slice_flatten)]
 use itertools::Itertools;
 use std::ops::Deref;
 use std::{collections::HashMap, fs::read_to_string};
@@ -13,71 +12,67 @@ struct Hand {
     position_value_map: Vec<u32>,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy)]
 enum HandType {
-    HighCard = 6,
-    OnePair = 5,
-    TwoPair = 4,
+    FiveOfAKind = 6,
+    FourOfAKind = 5,
+    FullHouse = 4,
     ThreeOfAKind = 3,
-    FullHouse = 2,
-    FourOfAKind = 1,
-    FiveOfAKind = 0,
+    TwoPair = 2,
+    OnePair = 1,
+    HighCard = 0,
 }
 
 fn main() {
     let lines = read_to_string("../aoc_day_7").expect("No File Found");
+    let sum = process(&lines).unwrap();
 
-    fn score_hand(hand: &str) -> (HandType, (u32, u32, u32, u32, u32)) {
-        use HandType::*;
+    println!("Sum: {}", sum);
+}
 
-        let counts = hand.chars().counts();
-        let values = counts.values().sorted().join("");
+fn score_hand(hand: &str) -> (HandType, (u32, u32, u32, u32, u32)) {
+    use HandType::*;
 
-        let hand_type = match values.deref() {
-            "5" => FiveOfAKind,
-            "14" => FourOfAKind,
-            "23" => FullHouse,
-            "113" => ThreeOfAKind,
-            "122" => TwoPair,
-            "1112" => OnePair,
-            "11111" => HighCard,
-            value => panic!("Something went wrong. Encountered `{}`", value),
-        };
+    let counts = hand.chars().counts();
+    let values = counts.values().sorted().join("");
+    let hand_type = match values.deref() {
+        "5" => FiveOfAKind,
+        "14" => FourOfAKind,
+        "23" => FullHouse,
+        "113" => ThreeOfAKind,
+        "122" => TwoPair,
+        "1112" => OnePair,
+        "11111" => HighCard,
+        value => panic!("should never happen. Encountered `{}`", value),
+    };
+    let card_scores = hand
+        .chars()
+        .map(|card| match card {
+            'A' => 14,
+            'K' => 13,
+            'Q' => 12,
+            'J' => 11,
+            'T' => 10,
+            value => value.to_digit(10).unwrap(),
+        })
+        .collect_tuple()
+        .unwrap();
+    (hand_type, card_scores)
+}
 
-        let card_scores_vec: Vec<u32> = hand
-            .chars()
-            .map(|c| match c {
-                'A' => 14,
-                'K' => 13,
-                'Q' => 12,
-                'J' => 11,
-                'T' => 10,
-                num => num.to_digit(10).unwrap(),
-            })
-            .collect();
-
-        (
-            hand_type,
-            (
-                card_scores_vec[0],
-                card_scores_vec[1],
-                card_scores_vec[2],
-                card_scores_vec[3],
-                card_scores_vec[4],
-            ),
-        )
-    }
-
-    let sum = lines
+pub fn process(input: &str) -> Result<String, String> {
+    let hands = input
         .lines()
         .map(|line| {
             let (hand, bid) = line.split_once(" ").unwrap();
-            (hand, bid.parse::<u32>().unwrap(), score_hand(hand))
+            let hand_bid_scores = (hand, bid.parse::<u32>().unwrap(), score_hand(hand));
+            println!("Hand Type: {:?}", hand_bid_scores.2 .0);
+            println!("Scores: {:?}", hand_bid_scores.2 .1);
+            hand_bid_scores
         })
         .sorted_by_key(|x| (x.2 .0 as u8, x.2 .1))
         .enumerate()
-        .map(|(index, (_hand, bid, _))| (index as u32 + 1) * bid)
+        .map(|(index, (_, bid, _))| (index as u32 + 1) * bid)
         .sum::<u32>();
-
-    println!("Sum: {}", sum);
+    Ok(hands.to_string())
 }
